3765c4163e1c194a6d67a76526663b95fdd13b78
zrdpsh <zerdposh@gmail.com>
2023-10-29 22:50:29 +0300

    project struture changed

---
+++ com/dwarfFortress/setByReference.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import com.dwarfFortress.*;
+
+public class setByReference{
+   public static void main(String[] args) throws FileNotFoundException {
+      Dwarf firstDwarf = new Dwarf("firstDwarf", 27);
+      secondDwarf = firstDwarf;
+   } //main
+} //class
