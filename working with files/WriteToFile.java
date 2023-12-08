9f1031c16d6d7adee1d65cb63ee8adc0dfd7c9ae
zrdpsh <zerdposh@gmail.com>
2023-12-08 23:55:53 +0300

    first part of program works as it should

--- working with files/WriteToFile.java
+++ working with files/WriteToFile.java
@@ -1,29 +1,28 @@
 import java.nio.file.*;
 import java.io.*;
-import static java.nio.file.StandartOpenOption;
-import 
+// import static java.nio.file.StandartOpenOption; 
+import java.lang.Math;
 
 public class WriteToFile {
    public static void main(String[] args) {
       bunchOfFiles(10, 3);
    } //public static void main
    
-   public static void bunchOfFiles(int howMuchFiles, int howMuchContent) throws IOException {
-      Random rand = new Math.random();
+   public static void bunchOfFiles(int howMuchFiles, int howMuchContent) {
       for (int i = 1; i <= howMuchFiles; i++) {
          try {
             String name = String.format("%s.txt", i);
-            File nfile = new File(String);
-            BufferedReader bw = new BufferedWriter(new FileWriter(nfile));
+            File nfile = new File(name);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(nfile));
             
             for (int j = 0; j < howMuchContent; j++) {
-               bw.write(""+rand.nextInt());
+               bw.write(""+Math.random());
                bw.newLine();
             } //loop j to write random numbers
             
             bw.close();
          } catch (IOException e) {
-            System.out.println("Error! - ", e);
+            System.out.println("Error! - "+e);
          }//try catch exception
       } //for loop inside function
    } //bunch of files function
