fafab479df293e87e3b0652640a59a0240d3156f
zrdpsh <zerdposh@gmail.com>
2023-12-09 14:02:18 +0300

    read from file function added

--- working with files/WriteToFile.java
+++ working with files/WriteToFile.java
@@ -2,6 +2,7 @@ import java.nio.file.*;
 import java.io.*;
 // import static java.nio.file.StandartOpenOption; 
 import java.lang.Math;
+import java.util.logging.Logger;
 
 public class WriteToFile {
    public static void main(String[] args) {
@@ -27,7 +28,41 @@ public class WriteToFile {
       } //for loop inside function
    } //bunch of files function
    
-   public static double sumOfFileContents(int num1, int num2, String pathToFiles) {
    
+   
+   
+   public static double openAndSumTheFiles(int num1, int num2, String pathToFiles) {
+      Logger logger = Logger.getLogger(WriteToFile.class.getName());
+      String file1name = pathToFiles+String.format("%s.txt", num1);
+      String file2name = pathToFiles+String.format("%s.txt", num2);
+      
+      try {
+         File
+         BufferedReader br = new BufferedReader(new FileReader(pathToFiles+file1name));
+
+      } catch(Exception e) {
+      
+      } finally {
+      
+      }
+      
    } //sumOfFilecontents function
+   
+   public static double[] sumOfFileContents(BufferedReader buffer) {
+      Logger logger = Logger.getLogger(WriteToFile.class.getName());
+      try {
+         String lineFromTheFile = buffer.nextLine();
+         return sumOfThreeNumbers(lineFromTheFile.split(" ");
+      } catch (Exception e) {
+         logger.log(e);
+      }
+   } //sum of file contents function
+   
+   private static double sumOfThreeNumbers(String[] numbersAsStrings) {
+      double resultingSum = 0;
+      for (int i = 0; i < numbersAsStrings.length; i++) {
+         result += (double)numbersAsStrings[i];
+      } //summation for loop
+      return resultingSum;
+   }//sum of three numbers
 }//Write to File class
