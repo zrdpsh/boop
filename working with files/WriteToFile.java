54b58ab03fbc2f4cb21dd3b49e2f13a0d3c3744f
zrdpsh <zerdposh@gmail.com>
2023-12-10 21:03:37 +0300

    WriteToFile working version

--- working with files/WriteToFile.java
+++ working with files/WriteToFile.java
@@ -2,77 +2,118 @@ import java.nio.file.*;
 import java.io.*;
 // import static java.nio.file.StandartOpenOption; 
 import java.lang.Math;
+import java.util.Arrays;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.util.Random;
 
 public class WriteToFile {
    public static void main(String[] args) {
       Random rand = new Random();
-      bunchOfFiles(10, 3);
-      double a = openAndSumTheFiles(rand.nextInt(11), rand.nextInt(11), "C:\\Users\\Admin\\Documents\\J_files\\boop\\boop\\working with files\\");
+      createBunchOfFiles(10, 3);
+      double a = sumTheContents("C:\\Users\\Admin\\Documents\\J_files\\boop\\boop\\working with files\\", rand.nextInt(10)+1, rand.nextInt(10)+1);
       System.out.println(a);
+      deleteTheFiles("C:\\Users\\Admin\\Documents\\J_files\\boop\\boop\\working with files\\");
    } //public static void main
    
-   public static void bunchOfFiles(int howMuchFiles, int howMuchContent) {
+   public static void createBunchOfFiles(int howMuchFiles, int howMuchContent) {
+      Logger logger = Logger.getLogger(WriteToFile.class.getName());
       for (int i = 1; i <= howMuchFiles; i++) {
-         try {
-            String name = String.format("%s.txt", i);
-            File nfile = new File(name);
-            BufferedWriter bw = new BufferedWriter(new FileWriter(nfile));
-            
+         String name = String.format("%s.txt", i);
+         File nfile = new File(name);
+         try (BufferedWriter bw = new BufferedWriter(new FileWriter(nfile))){
             for (int j = 0; j < howMuchContent; j++) {
-               bw.write(""+Math.random());
+               int someRandomNumber = (int)(Math.random()*10);
+               bw.write(""+someRandomNumber);
                bw.newLine();
+//               String loggerMessage = String.format("%s added to a file number %s", someRandomNumber, i);
+//               logger.log(Level.INFO, loggerMessage);
             } //loop j to write random numbers
-            
-            bw.close();
-         } catch (IOException e) {
-            System.out.println("Error! - "+e);
+         } catch (Exception e) {
+            logger.log(Level.INFO, "There is the exception", e);
          }//try catch exception
       } //for loop inside function
+      String loggerMessage = String.format("%s files created", howMuchFiles);
+      logger.log(Level.INFO, loggerMessage);
    } //bunch of files function
-   
-   
-   
-   
-   public static double openAndSumTheFiles(int num1, int num2, String pathToFiles) {
+
+   public static double sumTheContents(String pathToFiles, int... nums) {
+
       Logger logger = Logger.getLogger(WriteToFile.class.getName());
-      String file1name = pathToFiles+String.format("%s.txt", num1);
-      String file2name = pathToFiles+String.format("%s.txt", num2);
-      double sum1;
-      double sum2;
-      
-      try {
-         File fl1 = new File(pathToFiles+file1name);
-         BufferedReader br = new BufferedReader(new FileReader(fl1));
-         sum1 = sumOfThreeNumbers(br);
-         File fl2 = new File(pathToFiles+file2name);
-         BufferedReader br = new BufferedReader(new FileReader(fl2));
-         sum2 = sumOfThreeNumbers(br);
-      } catch(Exception e) {
-         logger.log(e);
-      } finally {
-         fl.close();
-      }
-         return sum1 + sum2;
-   } //sumOfFilecontents function
-   
-   public static double[] parseTheString(BufferedReader buffer) {
+      double resultingWholeSum = 0;
+
+      for (int i = 0; i < nums.length; i++) {
+         String fileName = pathToFiles+String.format("%s.txt", nums[i]);
+         try (FileReader fr = new FileReader(new File(fileName))) {
+               resultingWholeSum += sumOfReadNumbers(fr);
+               String loggerMessage = String.format("After the contents of the %s added, the resulting sum is %s", fileName, resultingWholeSum);
+               logger.log(Level.INFO, loggerMessage);
+         } catch (Exception e) {
+            logger.log(Level.SEVERE, "Contents of files didn't summed", e);
+         } // try catch block
+      } //for loop inside sumTheContents
+
+      return resultingWholeSum;
+
+   } //sumTheContents function
+
+   private static double sumOfReadNumbers(FileReader fr) {
       Logger logger = Logger.getLogger(WriteToFile.class.getName());
-      try {
-         String lineFromTheFile = buffer.nextLine();
-         return sumOfThreeNumbers(lineFromTheFile.split(" "));
-      } catch (Exception e) {
-         logger.log(e);
-      }
-   } //sum of file contents function
-   
-   private static double sumOfThreeNumbers(BufferedReader br) {
       double resultingSum = 0;
-      String[] numbersAsStrings = parseTheString(br);
-      for (int i = 0; i < numbersAsStrings.length; i++) {
-         result += (double)numbersAsStrings[i];
-      } //summation for loop
+
+      try (BufferedReader br = new BufferedReader(fr)) {
+         String[] contentsOfTheFile = parseTheStringFromFile(br);
+         String loggerMessage = String.format("While reading the file with SumOfReadNumbers:");
+         logger.log(Level.INFO, loggerMessage);
+         for (int i = 0; i < contentsOfTheFile.length; i++) {
+            resultingSum += Integer.parseInt(contentsOfTheFile[i]);
+            loggerMessage = String.format("we get %s after the %sst iteration", resultingSum, i);
+            logger.log(Level.INFO, loggerMessage);
+         }//summation for loop
+      } catch (Exception e){
+         logger.log(Level.SEVERE, "Numbers from file didn't summed", e);
+      }
       return resultingSum;
    }//sum of three numbers
+
+   private static String[] parseTheStringFromFile(BufferedReader buffer) throws IOException {
+      Logger logger = Logger.getLogger(WriteToFile.class.getName());
+      String linesFromTheFile = "";
+//      while (buffer.readLine()!=null) {
+//         lineFromTheFile += buffer.readLine();
+//         lineFromTheFile += " ";
+//      } //while buffer != null
+
+      try {
+         for (String lineRead = buffer.readLine(); lineRead != null; lineRead = buffer.readLine()) {
+            linesFromTheFile += lineRead;
+            linesFromTheFile += " ";
+         }
+      } catch (Exception e) {
+         logger.log(Level.INFO, e.toString());
+      }//try catch block
+
+      String loggerMessage = String.format("Inside the parseTheStringFromFile contents of file are %s", linesFromTheFile);
+      logger.log(Level.INFO, loggerMessage);
+      return linesFromTheFile.split(" ");
+   } //sum of file contents function
+
+   private static void deleteTheFiles(String pathToFiles){
+      Logger logger = Logger.getLogger(WriteToFile.class.getName());
+      logger.log(Level.ALL, "Deleting Files from 1 to 10:");
+      for (int i = 1; i < 11; i++) {
+         String pathToCurrentFile = pathToFiles + String.format("%s.txt", i);
+         String loggerMessage = String.format("removing %sst file", i);
+         logger.log(Level.ALL, loggerMessage);
+         try {
+            File fileToDelete = new File(pathToCurrentFile);
+            fileToDelete.delete();
+         } catch (Exception e) {
+            loggerMessage = String.format("while deleting file number %s, exception is thrown -", i);
+            logger.log(Level.ALL, loggerMessage);
+            logger.log(Level.ALL, e.toString());
+         }
+      }
+   }
+
 }//Write to File class
---
+++ working with files/WriteToFileTest.java
@@ -0,0 +1,22 @@
+class WriteToFileTest {
+    static String pathToFiles = "C:\\Users\\Admin\\Documents\\J_files\\boop\\boop\\working with files\\";
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void parseTheStringFromFile() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    double sumTheContents(String s, int i, int i1) {
+        double sumOf27and29 = sumTheContents(pathToFiles, 27, 29);
+        return sumOf27and29;
+    }
+}
