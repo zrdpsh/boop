cfc6bcada239e48cf6687b9f70d528c241bc3af1
zrdpsh <zerdposh@gmail.com>
2023-12-14 22:02:57 +0300

    Depth 10 updated updated

---
+++ files and folders/ExtensionsAndFoldersTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtensionsAndFoldersTest {
+
+    @org.junit.jupiter.api.Test
+
+
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnFilesAndFolderNames() {
+    }
+}
