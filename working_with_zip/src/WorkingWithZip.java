76208201b961d29e6b47b92a80d67cd0f820d58d
zrdpsh <zerdposh@gmail.com>
2023-12-18 11:31:41 +0300

    Working with Zip - version that run w/ errors

--- working_with_zip/src/WorkingWithZip.java
+++ working_with_zip/src/WorkingWithZip.java
@@ -3,19 +3,27 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 
 public class WorkingWithZip {
     public static void main(String[] args) throws IOException {
 
-        String[] tstString = createBunchOfFiles(10, 3);
+//        String[] tstString = createBunchOfFiles(10, 3);
 //        int[] zipResult = zipGivenFiles("tst.zip", tstString);
-        zipGivenFiles("tst.zip", tstString);
-        System.out.println(String.format("Function worked %s", zipResult[1] == 0? "FINE" : "BAD"));
+//        zipGivenFiles("tst.zip", tstString);
+//        System.out.println(String.format("Function worked %s", zipResult[1] == 0? "FINE" : "BAD"));
+
+//        saveFileInArchive("Infin.txt", "TestedArchive.zip");
+        String[] tstFiles = new String[]{"Infin.txt"};
+        addFilesToArchive("TestedArchive.zip", tstFiles);
+
     }
 
 
@@ -25,67 +33,110 @@ public class WorkingWithZip {
 * - сделать сам архив входящим потоком, открыть его на запись File -> */
     }
 
-    public static void saveFileInArchive(String fileToBeAdded, String archiveToAddAFile) throws FileNotFoundException {
-
-        Path pathToBeAdded = Paths.get((new File(fileToBeAdded)).getAbsolutePath());
-        ZipOutputStream zos = null;
-
-        try {
-            zos = new ZipOutputStream(new FileOutputStream(archiveToAddAFile));
-            zos.putNextEntry(new ZipEntry(fileToBeAdded));
-            /* открывает zip файл на запись и перемещает указатель в начало потока ??? */
-            Files.copy(pathToBeAdded, zos);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            zos.close();
-        }
-
-    }
-
-
-    public static int[] zipGivenFilesExp(String nameOfTheArchive, String[] namesOfTheFiles) throws IOException {
+    public static void addFilesToArchive(String nameOfTheArchive, String[] namesOfTheFiles) throws IOException {
         Logger logger = Logger.getLogger(WorkingWithZip.class.getName());
-        int[] rs = {1, 0};
-/*-----------------what this line do ?----------------*/
-        ZipOutputStream zipOtPt = new ZipOutputStream(new FileOutputStream(nameOfTheArchive));
-/*-----------------what this line do ?----------------*/
-        for (String fileName : namesOfTheFiles ) {
-/*-----------------what this line do ?----------------*/
-            zipFile(zipOtPt, fileName);
-/*-----------------what this line do ?----------------*/
-            logger.log(Level.INFO, String.format("%s is written to archive", fileName));
-        }//for fileName in namesOfTheFiles
 
-        ZipOutputStream zo =  new ZipOutputStream(new FileOutputStream(new File(nameOfTheArchive)));
-/*-----------------what this line do ?----------------*/
+        String pathZip = new File(nameOfTheArchive).getAbsolutePath();
 
+        Map<String, byte[]> zipEntryMap = link_FileName_With_ItsContents(pathZip);
+        FileOutputStream fileOutputStream =  new FileOutputStream(pathZip);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
 
-        return rs;
+        zipEntryMap.forEach((zipEntryName, bytes) -> {
+            System.out.println(zipEntryName+" "+bytes.toString());
+            try {
+                addExistingFile_ToTheNewArchive(pathZip, bytes, zipEntryName, zipOutputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        for (String fileName: namesOfTheFiles)  saveFileInArchive(fileName, pathZip, zipOutputStream);
+
+        zipOutputStream.close();
+
     }
-
-    public static void zipFile(ZipOutputStream zpOtPtStr, String fileName) throws IOException {
-        zpOtPtStr.putNextEntry(new ZipEntry(fileName));
-/*-----------------what this line do ?----------------*/
-        FileInputStream filInpStr = new FileInputStream(new File(fileName));
-/*-----------------what this line do ?----------------*/
-        byte[] buffer = new byte[4092];
-/*-----------------what this line do ?----------------*/
-        int byteCount = 0;
-/*-----------------what this line do ?----------------*/
-        while((byteCount = filInpStr.read(buffer)) != -1) {
-/*-----------------what this line do ?----------------*/
-            zpOtPtStr.write(buffer, 0, byteCount);
-/*-----------------what this line do ?----------------*/
-            System.out.flush();
-/*-----------------what this line do ?----------------*/
+        private static Map<String, byte[]> link_FileName_With_ItsContents(String pathZip) throws IOException {
+        Map<String, byte[]> zipEntryMap = new HashMap<>();
+        FileInputStream fileInputStream = new FileInputStream(pathZip);
+        ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
+        ZipEntry zipEntry;
+        while((zipEntry = zipInputStream.getNextEntry())!= null){
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream builder = new ByteArrayOutputStream();
+            int end;
+            while((end = zipInputStream.read(buffer)) > 0){
+                builder.write(buffer, 0, end);
+            }
+            zipEntryMap.put(zipEntry.getName(), builder.toByteArray());
         }
-
-        filInpStr.close();
-/*-----------------what this line do ?----------------*/
-        zpOtPtStr.closeEntry();
-/*-----------------what this line do ?----------------*/
+        return zipEntryMap;
     }
+
+    private static void addExistingFile_ToTheNewArchive(String pathZip, byte[] bytes, String zipEntryName, ZipOutputStream zipOutputStream) throws Exception{
+        ZipEntry zipEntry2nd = new ZipEntry(zipEntryName);
+        zipEntry2nd.setSize(bytes.length);
+        zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
+        zipOutputStream.write(bytes);
+    }
+
+    public static void addNewFileInOpenedArchive(Path fileToBeAdded, String pathToArchive, ZipOutputStream zos) throws IOException {
+        zos.putNextEntry(new ZipEntry("new/"+fileToBeAdded.getFileName()));
+        Files.copy(fileToBeAdded, zos);
+    }
+
+    public static void saveFileInArchive(String fileToBeAdded, String archiveToAddAFile, ZipOutputStream zos) throws IOException {
+
+        Path pathToAddedFile = Paths.get((new File(fileToBeAdded)).getAbsolutePath());
+
+        zos.putNextEntry(new ZipEntry((new File(fileToBeAdded)).getAbsolutePath()));
+        Files.copy(pathToAddedFile, zos);
+
+    }
+
+
+//    public static int[] zipGivenFilesExp(String nameOfTheArchive, String[] namesOfTheFiles) throws IOException {
+//        Logger logger = Logger.getLogger(WorkingWithZip.class.getName());
+//        int[] rs = {1, 0};
+///*-----------------what this line do ?----------------*/
+//        ZipOutputStream zipOtPt = new ZipOutputStream(new FileOutputStream(nameOfTheArchive));
+///*-----------------what this line do ?----------------*/
+//        for (String fileName : namesOfTheFiles ) {
+///*-----------------what this line do ?----------------*/
+//            zipFile(zipOtPt, fileName);
+///*-----------------what this line do ?----------------*/
+//            logger.log(Level.INFO, String.format("%s is written to archive", fileName));
+//        }//for fileName in namesOfTheFiles
+//
+//        ZipOutputStream zo =  new ZipOutputStream(new FileOutputStream(new File(nameOfTheArchive)));
+///*-----------------what this line do ?----------------*/
+//
+//
+//        return rs;
+//    }
+//
+//    public static void zipFile(ZipOutputStream zpOtPtStr, String fileName) throws IOException {
+//        zpOtPtStr.putNextEntry(new ZipEntry(fileName));
+///*-----------------what this line do ?----------------*/
+//        FileInputStream filInpStr = new FileInputStream(new File(fileName));
+///*-----------------what this line do ?----------------*/
+//        byte[] buffer = new byte[4092];
+///*-----------------what this line do ?----------------*/
+//        int byteCount = 0;
+///*-----------------what this line do ?----------------*/
+//        while((byteCount = filInpStr.read(buffer)) != -1) {
+///*-----------------what this line do ?----------------*/
+//            zpOtPtStr.write(buffer, 0, byteCount);
+///*-----------------what this line do ?----------------*/
+//            System.out.flush();
+///*-----------------what this line do ?----------------*/
+//        }
+//
+//        filInpStr.close();
+///*-----------------what this line do ?----------------*/
+//        zpOtPtStr.closeEntry();
+///*-----------------what this line do ?----------------*/
+//    }
 
 
     public static String[] createBunchOfFiles(int howMuchFiles, int howMuchContent) throws IOException {
