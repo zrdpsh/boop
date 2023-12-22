c4c962f6ec6554b39ab5f33e420108b7c1359a7e
zrdpsh <zerdposh@gmail.com>
2023-12-22 14:57:56 +0300

    draw function added

--- Working_with_images/src/WorkingWithImages.java
+++ Working_with_images/src/WorkingWithImages.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,29 +15,22 @@ public class WorkingWithImages {
 
     public static void main(String[] args) throws IOException {
         logger.log(Level.INFO, String.format("main method started"));
-        ArrayList<File> convertedImages = returnConvertedImages("png", "jpg");
+        ArrayList<File> convertedImages = returnConvertedImages("png", "jpg", 2, true);
         logger.log(Level.INFO, String.format("There are %s converted files", convertedImages.size()));
 
     }
 
-    public static ArrayList<File> returnConvertedImages(String givenFormat, String desiredFormat) throws IOException {
+    public static ArrayList<File> returnConvertedImages(String givenFormat, String desiredFormat, int depth, boolean addGraphics) throws IOException {
         logger.log(Level.INFO, "-------------------------------");
         logger.log(Level.INFO, "returnConvertedImages is called");
         logger.log(Level.INFO, "-------------------------------");
 
-        File root = new File(".");
-        /* инициализировать файл с именем "." - верхняя папка в иерархии */
-
         ArrayList<File> expand = new ArrayList<File>();
-        /* объявить список ArrayList, который называется expand - в него можно записать произвольное количество File" */
-
         ArrayList<File> result  = new ArrayList<>();
 
-        expand.add(root);
-        /* добавить в ArrayList File, созданный в первой строчке */
+        expand.add(new File(System.getProperty("user.dir")));
 
-        for(int depth = 0; depth < 10; depth++) {
-            /* запустить цикл на 10 повторений - число равно нужной глубине вложенности */
+        for(int u = 0; u < depth; u++) {
 
             File[] expandCopy = expand.toArray(new File[expand.size()]);
             /* переписываем данные из списка expand в массив expandCopy
@@ -46,58 +40,70 @@ public class WorkingWithImages {
             expand.clear();
             /* освободить первоначальный список для новых названий папок. старые - из предыдущего уровня - остаются в только что созданной копии */
             for (File givenFile : expandCopy) {
-                logger.log(Level.INFO, String.format("main loop runs through %s sublevel", depth));
+                logger.log(Level.INFO, String.format("we are searching %s level deep in hierarchy", depth));
                 if (givenFile.isDirectory()) {
-                    /* проверить, является ли файл из полученного списка папкой - так мы получаем список файлов следующего уровня вложенности... */
                     expand.addAll(Arrays.asList(givenFile.listFiles()));
                     /* .. если является - получаем файлы, лежащие внутри file c помощью .listFiles() в виде массива с объектами File ->
                      *   -> Arrays.asList превращает наш массив файлов в список
                      *   добавляем названия файлов в expand, освобождённый перед началом работы цикла*/
                 } else {
-//                    String[] t = getNameAndExtension(file);
-//                    logger.log(Level.INFO, String.format("we tried to separate name and extension " +
-//                            "of the given file and write them into temporary variable"));
-//
-//                    String givenFile = t[0];
-//                    String extensionOfTheGivenFile = t[1];
                     String extensionOfTheGivenFile = getExtension(givenFile.getName());
                     logger.log(Level.INFO, String.format("we have %s file named %s", extensionOfTheGivenFile, givenFile.getName()));
 
-                    if ((extensionOfTheGivenFile != null) && (extensionOfTheGivenFile.equalsIgnoreCase(desiredFormat))) {
-                        result.add(resultOfConverting(givenFile.getName(), desiredFormat));
+                    if ((extensionOfTheGivenFile != null) && (extensionOfTheGivenFile.equalsIgnoreCase(givenFormat))) {
+                        result.add(resultOfConverting(givenFile.getName(), desiredFormat,addGraphics));
                         logger.log(Level.INFO, "Given file do have needed extension. Adding it to resulting array");
                     }
                 } //else clause inside inner loop
-                /* на самой первой итерации папка будет всего одна - "." */
             } // inner loop to get all the files
         } //main loop inside converted images
         return result;
     } //return converted images
 
-    private static File resultOfConverting(String nameOfTheGivenFile, String desiredFormat) throws IOException {
+    private static File resultOfConverting(String nameOfTheGivenFile, String desiredFormat, boolean drawImage) throws IOException {
+        logger.log(Level.INFO, "-------------------------------");
+        logger.log(Level.INFO, String.format("getExtension is called with file %s from returnConvertedImages", nameOfTheGivenFile));
+        logger.log(Level.INFO, "-------------------------------");
+
         File resultingImage;
 
         try {
-            logger.log(Level.INFO, String.format("converting image"));
+            logger.log(Level.INFO, String.format("try to convert image"));
             BufferedImage img = ImageIO.read(new File(nameOfTheGivenFile));
+            if (drawImage) drawSquare(img, "Hello,\n World!");
             resultingImage = new File(nameOfTheGivenFile.substring(0,nameOfTheGivenFile.lastIndexOf(".")) + "." + desiredFormat);
             ImageIO.write(img, desiredFormat, resultingImage);
             return resultingImage;
         } catch (Exception e) {
-            logger.log(Level.INFO, "Exeption while read write file");
+            logger.log(Level.INFO, "Exception while read write file");
             throw e;
         }
     }//return converted File
 
     private static String getExtension(String fileName) {
-        logger.log(Level.INFO, "getNameAndExtension is called");
-        logger.log(Level.INFO, String.format("Inside getNameAndExtension we get %s extension", fileName));
+        logger.log(Level.INFO, "-------------------------------");
+        logger.log(Level.INFO, String.format("getExtension is called with file %s from returnConvertedImages", fileName));
+        logger.log(Level.INFO, "-------------------------------");
         int i = fileName.lastIndexOf('.');
         if (i > 0) {
-            logger.log(Level.INFO, String.format("last index of is %s", i));
+            logger.log(Level.INFO, String.format("file have extension '.%s'", fileName.substring(i+1)));
             return fileName.substring(i+1);
         }
         logger.log(Level.INFO, String.format("Using getExtension_m we find nothing", fileName));
         return null;
     }
+
+    private static void drawSquare(BufferedImage img, String textInsideTheFrame) throws IOException {
+        Graphics2D g = img.createGraphics();
+        int s = img.getWidth() > img.getHeight()? img.getHeight()/3 : img.getWidth()/3;
+        g.setColor(Color.BLACK);
+        g.fillRect((img.getWidth()-s)/2, (img.getHeight()-s)/2, s, s);
+        logger.log(Level.INFO, String.format("bacground added"));
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial Black", Font.ITALIC, 20));
+        g.drawString(textInsideTheFrame, img.getWidth()/3,img.getHeight()/3);
+        logger.log(Level.INFO, String.format("text added"));
+        g.dispose();
+        g.dispose();
+    }//drawSquare
 }
